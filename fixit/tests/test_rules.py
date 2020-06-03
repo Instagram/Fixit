4fc46e0d9caea1ffafff10db96b8fd2dc31b8e49
Josephine Eshkenazi <josieesh@fb.com>
2020-06-03 13:19:44 -0400

    Reworked the lint rule testing classes. Work in progress

--- fixit/tests/test_rules.py
+++ fixit/tests/test_rules.py
@@ -3,58 +3,184 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import TYPE_CHECKING, Mapping, Type, Union
+import copy
+import re
+import textwrap
+import unittest
+from dataclasses import dataclass
+from functools import wraps
+from pathlib import Path
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    Dict,
+    Mapping,
+    Optional,
+    Sequence,
+    Tuple,
+    Type,
+    Union,
+)
 
 from libcst.testing.utils import data_provider
 from mypy_extensions import TypedDict
 
 from fixit.common.base import CstLintRule
-from fixit.common.testing import LintRuleTest
-from fixit.rule_lint_engine import get_rules
+from fixit.common.report import BaseLintRuleReport
+from fixit.common.utils import InvalidTestCase, ValidTestCase  # noqa: F401
+from fixit.rule_lint_engine import get_rules, lint_file
 
 
-if TYPE_CHECKING:
-    from fixit.common.utils import ValidTestCase, InvalidTestCase  # noqa: F401
-
-
-class TestCase(TypedDict):
-    test_case: Union["ValidTestCase", "InvalidTestCase"]
+@dataclass(frozen=True)
+class TestCase:
     rule: Type[CstLintRule]
+    test_methods: Mapping[str, Union[ValidTestCase, InvalidTestCase]]
 
 
-def _gen_test_cases() -> Mapping[str, TestCase]:
-    cases = {}
-    for rule in get_rules():
-        if not issubclass(rule, CstLintRule):
-            continue
-        if hasattr(rule, "VALID"):
-            # pyre-ignore[16]: `CstLintRule` has no attribute `VALID`.
-            for idx, test_case in enumerate(rule.VALID):
-                cases[f"{rule.__name__}_VALID_{idx}"] = {
-                    "test_case": test_case,
-                    "rule": rule,
-                }
-        if hasattr(rule, "INVALID"):
-            # pyre-ignore[16]: `CstLintRule` has no attribute `INVALID`.
-            for idx, test_case in enumerate(rule.INVALID):
-                cases[f"{rule.__name__}_INVALID_{idx}"] = {
-                    "test_case": test_case,
-                    "rule": rule,
-                }
-    return cases
+class BaseTestMeta(type):
+    def __new__(cls, name: str, bases: Tuple[type, ...], dct: Dict[str, Any]) -> object:
+        test_cases: Sequence[TestCase] = cls._gen_test_cases()
+        cls.populate_data_provider_tests(dct, test_cases)
+        return super().__new__(cls, name, bases, dict(dct))
+
+    @classmethod
+    def populate_data_provider_tests(cls, dct, test_cases: Sequence[TestCase]) -> None:
+        test_cases_to_add: Dict[str, unittest.TestCase] = {}
+        for test_case in test_cases:
+            rule_name = test_case.rule.__name__
+            test_methods_to_add: Dict[str, Callable] = dict()
+
+            for test_method_name, test_method_data in test_case.test_methods.items():
+                # member = dct["_test_case"]
+
+                # @wraps(member)
+                def test_method(
+                    self: object,
+                    data: Union[ValidTestCase, InvalidTestCase] = test_method_data,
+                    rule: Type[CstLintRule] = test_case.rule,
+                ) -> object:
+                    return self._test_case(data, rule)
+
+                test_method.__name__ = test_method_name
+                test_methods_to_add[test_method_name] = test_method
+
+            test_case_type = type(
+                rule_name, (LintRuleTestCaseStub,), test_methods_to_add
+            )
+            test_cases_to_add.update({rule_name: test_case_type})
+        dct.update(test_cases_to_add)
+
+    @classmethod
+    def _gen_test_cases(cls) -> Sequence[TestCase]:
+        cases = []
+        for rule in get_rules():
+            if not issubclass(rule, CstLintRule):
+                continue
+            test_cases_for_rule = cls._gen_test_case(rule)
+            cases.append(test_cases_for_rule)
+        return cases
+
+    @classmethod
+    def _gen_test_case(cls, rule) -> TestCase:
+        valid_tcs = dict()
+        invalid_tcs = dict()
+        if issubclass(rule, CstLintRule):
+
+            if hasattr(rule, "VALID"):
+                # pyre-ignore[16]: `CstLintRule` has no attribute `VALID`.
+                for idx, test_case in enumerate(rule.VALID):
+                    valid_tcs[f"test_VALID_{idx}"] = test_case
+            if hasattr(rule, "INVALID"):
+                # pyre-ignore[16]: `CstLintRule` has no attribute `INVALID`.
+                for idx, test_case in enumerate(rule.INVALID):
+                    invalid_tcs[f"test_INVALID_{idx}"] = test_case
+        return TestCase(rule=rule, test_methods={**valid_tcs, **invalid_tcs})
 
 
-class Test(LintRuleTest):
-    @data_provider(_gen_test_cases(), test_limit=10000)
-    def test(
-        self,
-        test_case: Union["ValidTestCase", "InvalidTestCase"],
-        rule: Type[CstLintRule],
+class LintRuleTestCaseStub(unittest.TestCase):
+    @staticmethod
+    def _dedent(src: str) -> str:
+        src = re.sub(r"\A\n", "", src)
+        return textwrap.dedent(src)
+
+    @staticmethod
+    def _validate_patch(
+        report: BaseLintRuleReport, test_case: InvalidTestCase
+    ) -> unittest.TestResult:
+        patch = report.patch
+        expected_replacement = test_case.expected_replacement
+
+        if patch is None:
+            if expected_replacement is not None:
+                raise AssertionError(
+                    "The rule for this test case has no auto-fix, but expected source was specified."
+                )
+            return
+
+        if expected_replacement is None:
+            raise AssertionError(
+                "The rule for this test case has an auto-fix, but no expected source was specified."
+            )
+
+        expected_replacement = _dedent(expected_replacement)
+        patched_code = patch.apply(_dedent(test_case.code))
+        if patched_code != expected_replacement:
+            raise AssertionError(
+                "Auto-fix did not produce expected result.\n"
+                + f"Expected:\n{expected_replacement}\n"
+                + f"But found:\n{patched_code}"
+            )
+
+    def _test_case(
+        self, test_case: Union[ValidTestCase, InvalidTestCase], rule: Type[CstLintRule],
     ) -> None:
-        self._test_rule(test_case, rule=rule)
+        reports = lint_file(
+            Path(test_case.filename),
+            LintRuleTestCaseStub._dedent(test_case.code).encode("utf-8"),
+            config=test_case.config,
+            rules=[rule],
+        )
+        if isinstance(test_case, ValidTestCase):
+            self.assertEqual(
+                len(reports),
+                0,
+                'Expected zero reports for this "valid" test case. Instead, found:\n'
+                + "\n".join(str(e) for e in reports),
+            )
+        else:
+            self.assertGreater(
+                len(reports),
+                0,
+                'Expected a report for this "invalid" test case. `self.report` was not called.',
+            )
+            self.assertLessEqual(
+                len(reports),
+                1,
+                'Expected one report from this "invalid" test case. Found multiple:\n'
+                + "\n".join(str(e) for e in reports),
+            )
 
-    # pyre-ignore[14]: `test_rule` overrides method defined in `LintRuleTest`
-    #  inconsistently.
-    def test_rule(self) -> None:
-        # explicit empty test case to overwrite parent's implementation
-        pass
+            # pyre-fixme[16]: `Collection` has no attribute `__getitem__`.
+            report = reports[0]
+
+            if not (test_case.line is None or test_case.line == report.line):
+                raise AssertionError(
+                    f"Expected line: {test_case.line} but found line: {report.line}"
+                )
+
+            if not (test_case.column is None or test_case.column == report.column):
+                raise AssertionError(
+                    f"Expected column: {test_case.column} but found column: {test_case.column}"
+                )
+
+            if test_case.kind != report.code:
+                raise AssertionError(
+                    f"Expected:\n    {test_case.expected_str}\nBut found:\n    {report}"
+                )
+
+            LintRuleTestCaseStub._validate_patch(report, test_case)
+
+
+class TestLintRules(unittest.TestSuite, metaclass=BaseTestMeta):
+    pass
