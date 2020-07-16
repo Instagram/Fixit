42ab0a59a65dcc4c068041a3d4ca61cdd3bdc35f
Josie Eshkenazi <eshkenazi.josephine@gmail.com>
2020-07-16 15:49:55 -0400

    Test with multiple paths

--- fixit/common/cli/tests/test_lint_opts.py
+++ fixit/common/cli/tests/test_lint_opts.py
@@ -36,24 +36,47 @@ class FakeLintSuccessReport(LintSuccessReportBase):
     def create_reports(
         path: Path, reports: Collection[BaseLintRuleReport], global_list: List[str],
     ) -> Sequence[LintSuccessReportBase]:
-        global_list.append("anything")
+        global_list.append(str(path))
         return [FakeLintSuccessReport(str(path), "success", ["fake picklable report"])]
 
 
 class LintOptsTest(UnitTest):
-    @patch("builtins.open", mock_open(read_data=b"test"))
-    def test_opts_with_extra(self) -> None:
-        global_list = []
-        opts = LintOpts(
+    def setUp(self) -> None:
+        self.global_list = []
+        self.opts = LintOpts(
             [FakeRule],
             FakeLintSuccessReport,
             LintFailureReportBase,
-            extra={"global_list": global_list},
+            extra={"global_list": self.global_list},
         )
-        json_results = get_file_lint_result_json(Path(__file__), opts)
+
+    @patch("builtins.open", mock_open(read_data=b"test"))
+    def test_opts_with_extra(self) -> None:
+        path = Path("fake/path.py")
+        json_results = get_file_lint_result_json(path, self.opts)
+
         # Assert global list has been modified
-        self.assertEqual(list(global_list), ["anything"])
+        self.assertEqual(list(self.global_list), [str(path)])
+
         # Assert the rest of the reporting functionality is as expected
         self.assertEqual(len(json_results), 1)
         json_for_file = json.loads(json_results[0])
         self.assertEqual(json_for_file["reports"][0], "fake picklable report")
+
+    @patch("builtins.open", mock_open(read_data=b"test"))
+    def test_opts_with_extra_multiple_paths(self) -> None:
+        paths = [Path("fake/path1.py"), Path("fake/path2.py")]
+        json_results_path1 = get_file_lint_result_json(paths[0], self.opts)
+        json_results_path2 = get_file_lint_result_json(paths[1], self.opts)
+
+        # Assert global list has been modified as expected
+        self.assertEqual(list(self.global_list), [str(p) for p in paths])
+
+        # Assert the rest of the reporting functionality is as expected
+        self.assertEqual(len(json_results_path1), 1)
+        json_for_file_1 = json.loads(json_results_path1[0])
+        self.assertEqual(json_for_file_1["reports"][0], "fake picklable report")
+
+        self.assertEqual(len(json_results_path2), 1)
+        json_for_file_2 = json.loads(json_results_path2[0])
+        self.assertEqual(json_for_file_2["reports"][0], "fake picklable report")
