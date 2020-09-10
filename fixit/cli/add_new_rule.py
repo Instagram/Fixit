a10b663de504d62a49021e38727e75e4fa26112c
Charles Patel <charlespatel07@gmail.com>
2020-09-10 15:15:01 -0700

    Script to add new rule file (#123)
    * Added a script to create rule
    
    * Update gitignore
    
    * Updated gitignore
    
    * Removed untracked files
    
    * Lint
    
    * Minor fixes
    
    * Minor fixes to new file
    
    * Lint fixes to new file
    
    * Lint fixes
    
    * Removed new rule file
    
    * Comments formatting
    
    * Sorted imports
    
    * Removed new rule file
    
    * Updated ipynb documentation
    
    * Removed redundant cells
    
    * Execution count to null
    
    * Updated getting started doc
    
    * Minor fixes
    
    * Removed execution count
    
    * Clear outputs from ipynb
    
    * Replaced class from build file
    
    * Exception correction
    
    * Minor Fixes

---
+++ fixit/cli/add_new_rule.py
@@ -0,0 +1,95 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# Usage:
+#
+#   $ python -m fixit.cli.add_new_rule --help
+#   $ python -m fixit.cli.add_new_rule
+#   $ python -m fixit.cli.add_new_rule --path fixit/rules/new_rule.py
+
+import argparse
+from pathlib import Path
+
+from libcst.codemod._cli import invoke_formatter
+
+from fixit.common.config import get_lint_config
+
+
+_LICENCE = """# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""
+
+_IMPORTS = """
+import libcst as cst
+import libcst.matchers as m
+
+from fixit import CstLintRule, InvalidTestCase as Invalid, ValidTestCase as Valid
+
+"""
+
+_TO_DOS = """
+\"""
+This is a model rule file for adding a new rule to fixit module
+\"""
+
+"""
+
+_RULE_CLASS = """
+class Rule(CstLintRule):
+    \"""
+    docstring or new_rule description
+    \"""
+
+    MESSAGE = "Enter rule description message"
+
+    VALID = [Valid("'example'")]
+
+    INVALID = [Invalid("'example'")]
+"""
+
+
+def is_path_exists(path: str) -> Path:
+    """Check for valid path, if yes, return `Path` else raise `Error` """
+    filepath = Path(path)
+    if filepath.exists():
+        raise FileExistsError(f"{filepath} already exists")
+    elif not filepath.parent.exists():
+        raise TypeError(f"{filepath} is not a valid path, provide path with file name")
+    else:
+        return filepath
+
+
+def create_rule_file(file_path: Path) -> None:
+    """Create a new rule file."""
+    context = _LICENCE + _IMPORTS + _TO_DOS + _RULE_CLASS
+    updated_context = invoke_formatter(get_lint_config().formatter, context)
+
+    with open(file_path, "w") as f:
+        f.write(updated_context)
+
+    print(f"Successfully created {file_path.name} rule file at {file_path.parent}")
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description="Creates a skeleton of adding new rule file"
+    )
+    parser.add_argument(
+        "-p",
+        "--path",
+        type=is_path_exists,
+        default=Path("fixit/rules/new_rule.py"),
+        help="Path to add rule file, Default is fixit/rules/new_rule.py",
+    )
+
+    args = parser.parse_args()
+    create_rule_file(args.path)
+
+
+if __name__ == "__main__":
+    main()
