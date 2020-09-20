3772a2f5a92303dfc8fc4dbc8a3d3678319b6001
Charles <charlespatel07@gmail.com>
2020-09-19 18:47:43 -0700

    Comment changes

--- fixit/cli/add_new_rule.py
+++ fixit/cli/add_new_rule.py
@@ -95,7 +95,7 @@ def main() -> None:
         "--path",
         type=is_path_exists,
         default=Path("fixit/rules/new.py"),
-        help="Path to add rule file, Default is fixit/rules/new.py",
+        help="Path to add rule file, defaults to fixit/rules/new.py",
     )
 
     parser.add_argument(
@@ -103,7 +103,7 @@ def main() -> None:
         "--name",
         type=str,
         default="",
-        help="Name of the rule, Default is New",
+        help="Name of the rule, defaults to `New`",
     )
 
     args = parser.parse_args()
