04f9d685e49bfc3617967f42205b958907e0d12d
Alain Hernandez <alainehernandez@gmail.com>
2021-02-24 22:41:57 -0500

    Mock-out LintOpts for reusage

--- fixit/cli/tests/test_lint_opts.py
+++ fixit/cli/tests/test_lint_opts.py
@@ -37,6 +37,22 @@ class FakeLintSuccessReport(LintSuccessReportBase):
         return [FakeLintSuccessReport(str(path), "success", ["fake picklable report"])]
 
 
+@dataclass(frozen=True)
+class FakeLintFailureReport(LintFailureReportBase):
+    path: str
+    status: str
+    reports: Collection[str]
+
+    @staticmethod
+    def create_reports(
+        path: Path,
+        reports: Collection[BaseLintRuleReport],
+        global_list: List[str],
+    ) -> Sequence["FakeLintFailureReport"]:
+        global_list.append(str(path))
+        return [FakeLintFailureReport(str(path), "failure", ["fake picklable report"])]
+
+
 class FakeRule(CstLintRule):
     def visit_Module(self, node: Module) -> None:
         self.report(node, "Dummy message")
@@ -55,15 +71,22 @@ def mock_operation(
     return cast(Sequence[FakeLintSuccessReport], results)
 
 
+def generate_mock_lint_opt(global_list=None):
+    if global_list is None:
+        global_list = []
+
+    return LintOpts(
+        {FakeRule},
+        FakeLintSuccessReport,
+        FakeLintFailureReport,
+        extra={"global_list": global_list},
+    )
+
+
 class LintOptsTest(UnitTest):
     def setUp(self) -> None:
         self.global_list = []
-        self.opts = LintOpts(
-            {FakeRule},
-            FakeLintSuccessReport,
-            LintFailureReportBase,
-            extra={"global_list": self.global_list},
-        )
+        self.opts = generate_mock_lint_opt(global_list=self.global_list)
 
     def test_extra_opts(self) -> None:
         path = "path.py"
