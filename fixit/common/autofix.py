13483782a89346fb98399c4e3ab1c4d1ac931a9c
Lisa Roach <lisroach@fb.com>
2021-04-22 16:38:34 -0700

    Fixes broken type hints.

--- fixit/common/autofix.py
+++ fixit/common/autofix.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass
 from typing import Union
 
 import libcst as cst
+from libcst import FlattenSentinel
 from libcst.metadata import (
     CodePosition,
     ExperimentalReentrantCodegenProvider,
@@ -34,7 +35,7 @@ class LintPatch:
     def get(
         wrapper: MetadataWrapper,
         original_node: cst.CSTNode,
-        replacement_node: Union[cst.CSTNode, cst.RemovalSentinel],
+        replacement_node: Union[cst.CSTNode, FlattenSentinel, cst.RemovalSentinel],
     ) -> "LintPatch":
         # Batch the execution of these position providers
         wrapper.resolve_many(
@@ -172,9 +173,11 @@ class LintPatch:
 def _replace_or_remove(
     parent: cst.CSTNode,
     original_node: cst.CSTNode,
-    replacement_node: Union[cst.CSTNode, cst.RemovalSentinel],
+    replacement_node: Union[cst.CSTNode, cst.FlattenSentinel, cst.RemovalSentinel],
 ) -> cst.CSTNode:
-    if isinstance(replacement_node, cst.RemovalSentinel):
+    if isinstance(replacement_node, cst.RemovalSentinel) or isinstance(
+        replacement_node, cst.FlattenSentinel
+    ):
         return cst.ensure_type(parent.deep_remove(original_node), cst.CSTNode)
     else:
         return parent.deep_replace(original_node, replacement_node)
