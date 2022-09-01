c95b0ef9f8c02adfd6a541b55f22f0bd6a922706
Amethyst Reese <amy@n7.gg>
2022-09-01 16:53:13 -0700

    Config loading and hierarchical merging (#223)
    First piece of config loading based on the proposal in https://github.com/amyreese/Fixit/tree/config-prototype
    
    - Walks the tree up from a target path to either a config with `root = true` or the top of the drive/filesystem.
    - Loads all discovered files using tomllib/tomli.
    - Basic priority-based overrides of values from outer configs.
    
    Future work will add support for more intelligent merging of values rather than strict overrides, as well as support for multiple path-based overrides in a single config.

--- src/fixit/types.py
+++ src/fixit/types.py
@@ -3,9 +3,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
-from typing import List, Optional
+from typing import Any, Dict, List, Optional
 
 from libcst._add_slots import add_slots
 from libcst.metadata import CodeRange
@@ -30,8 +30,18 @@ class Config:
     """
 
     path: Path
-    enable: List[str]
-    disable: List[str]
+    root: Path
+
+    enable: List[str] = field(default_factory=lambda: ["fixit.rules"])
+    disable: List[str] = field(default_factory=list)
+
+    greeting: str = "hello"
+
+
+@dataclass
+class RawConfig:
+    path: Path
+    data: Dict[str, Any]
 
 
 @add_slots
