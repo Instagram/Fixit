dd962d98e39f3c2aa4c46ac6b726385a8ca99307
Zsolt Dollenstein <zsol.zsol@gmail.com>
2022-09-05 15:48:53 +0100

    reformat

--- src/fixit/tests/__init__.py
+++ src/fixit/tests/__init__.py
@@ -3,6 +3,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .rule.cst import RunnerTest
 from .config import ConfigTest
+from .rule.cst import RunnerTest
 from .smoke import SmokeTest
