cdcc00ed07f318dde53782aefd16bc6ad76372df
Amethyst Reese <amy@n7.gg>
2023-03-13 12:49:05 -0700

    Pass correct list of metadata providers to repo manager
    [ghstack-poisoned]

--- src/fixit/rule/cst.py
+++ src/fixit/rule/cst.py
@@ -87,7 +87,7 @@ class CSTLintRunner(LintRunner["CSTLintRule"]):
             repo_manager = FullRepoManager(
                 repo_root_dir=config.root.as_posix(),
                 paths=[config.path.as_posix()],
-                providers={FullyQualifiedNameProvider},
+                providers=needs_repo_manager,
             )
             repo_manager.resolve_cache()
             metadata_cache = repo_manager.get_cache_for_path(config.path.as_posix())
