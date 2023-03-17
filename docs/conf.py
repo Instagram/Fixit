d9e62266473fa013dec8e94d2d67c487bbe3ede6
Michel Alexandre Salim <salimma@fedoraproject.org>
2023-03-17 16:32:48 -0500

    Remove unused Sphinx inventories
    These are not used, and `stdlib_list` has not been updated since 2015.
    
    After removing, documentation builds fine for the upcoming update of the
    Fedora package.
    
    Signed-off-by: Michel Alexandre Salim <salimma@fedoraproject.org>

--- docs/conf.py
+++ docs/conf.py
@@ -57,8 +57,6 @@ autodoc_member_order = "groupwise"
 highlight_language = "python3"
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
-    "pathspec": ("https://python-path-specification.readthedocs.io/en/latest/", None),
-    "stdlib_list": ("https://python-stdlib-list.readthedocs.io/en/latest/", None),
 }
 master_doc = "index"
 
